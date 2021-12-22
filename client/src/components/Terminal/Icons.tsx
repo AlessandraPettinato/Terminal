d8728b106e15075f556981aab17b32ab2e13274e
Alessandra Pettinato <alessandrapettinato@hotmail.it>
2021-12-22 15:48:39 +0100

    feat: override style of react icons svg and include them on terminal bar

---
+++ client/src/components/Terminal/Icons.tsx
@@ -0,0 +1,45 @@
+export const AiFillCloseCircle = ({
+	fill,
+	stroke,
+	height,
+	width,
+	className,
+}: any) => {
+	return (
+		<svg
+			stroke={stroke}
+			fill={fill}
+			stroke-width="0"
+			viewBox="0 0 1024 1024"
+			height={height}
+			width={width}
+			xmlns="http://www.w3.org/2000/svg"
+			className={className}
+		>
+			<path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 0 1-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"></path>
+		</svg>
+	);
+};
+
+export const IoMdRemoveCircle = ({
+	fill,
+	stroke,
+	height,
+	width,
+	className,
+}: any) => {
+	return (
+		<svg
+			stroke={stroke}
+			fill={fill}
+			stroke-width="0"
+			viewBox="0 0 512 512"
+			height={height}
+			width={width}
+			xmlns="http://www.w3.org/2000/svg"
+			className={className}
+		>
+			<path d="M256 48C141.125 48 48 141.125 48 256s93.125 208 208 208 208-93.125 208-208S370.875 48 256 48zm107 229H149v-42h214v42z"></path>
+		</svg>
+	);
+};
--- client/src/components/Terminal/Terminal.tsx
+++ client/src/components/Terminal/Terminal.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import Draggable from "react-draggable";
 import { ResizableBox } from "react-resizable";
+import { AiFillCloseCircle, IoMdRemoveCircle } from "./Icons";
 
 import { GET_ALL_FILES } from "../../queries/FileQueries";
 import { FileType } from "../../types/FileType";
@@ -38,37 +39,52 @@ const Terminal: React.FC = () => {
 
 	if (loading) <p>Loading...</p>;
 	if (error) <p>Somethins went wrong: {error.message}</p>;
+
 	return (
 		<Draggable
 			handle=".handle"
 			scale={1}
-			position={null || undefined}
+			// position={null || undefined}
 			bounds={{
 				left: 0,
 				top: 0,
 				right: window.innerWidth - size.width,
 				bottom: window.innerHeight - size.height - 80,
 			}}
-			defaultPosition={{ x: 0, y: 0 }}
+			defaultPosition={{ x: 10, y: 10 }}
 		>
 			<ResizableBox
 				width={size.width}
 				height={size.height}
 				lockAspectRatio={true}
 				minConstraints={[640, 480]}
-				className="box"
+				className="terminal-window"
 				maxConstraints={[
 					window.innerWidth,
 					window.innerHeight - size.height - 80,
 				]}
 				onResize={handleResize}
 			>
-				<div className="terminal-window">
-					<header className="login">{login}</header>
+				<div className="bar">
+					<AiFillCloseCircle
+						fill="#ff5f57"
+						stroke="#ff5f57"
+						height="0.7rem"
+						width="0.7rem"
+						className="icon"
+					/>
+					<IoMdRemoveCircle
+						fill="#febc2e"
+						stroke="#febc2e"
+						height="0.7rem"
+						width="0.7rem"
+						className="icon"
+					/>
+				</div>
+				<div className="login">{login}</div>
 
-					<div className="user-container">
-						<UserInput width={size.width} height={size.height} files={files} />
-					</div>
+				<div className="user-container">
+					<UserInput width={size.width} height={size.height} files={files} />
 				</div>
 			</ResizableBox>
 		</Draggable>
